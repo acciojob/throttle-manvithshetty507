3a6464ce680bb5dc8b82c30414579b897f9c2e8a
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-16 14:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 //your JS code here. If required.
 function throttle(callback, delay) {
+	let waitingargs;
+	let shouldWait;
+	let timeoutId;
 
+	function throttler(...args) {
+		if(shouldWait) {
+			callback.call(this,args)
+			shouldWait = false
+			
+			timeoutId = setTimeout(() => {
+                shouldWait = true
+                if (waitingArgs) {
+                    // If there are waiting arguments, execute the callback with them
+                    throttler.apply(this, waitingArgs)
+                    waitingArgs = null
+                }
+            }, delay);
+		} else {
+            // If shouldWait is false, save the arguments for later execution
+            waitingArgs = args
+        }
+	}
+	return throttler
 }
 
 module.exports = throttle;
